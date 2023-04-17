da62b784ac97b5d320d2ad65d7779292c4004fae
IVAN <vanay.stuk10@mail.ru>
2023-04-17 20:03:31 +0300

    yeyseys

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
-a = [3,5,6,9,1,8,]
-k = sorted(a)
-b = 0
-c = len(a)
-g = 0
-for i in a:
-    b = b + i
-g = b / c
-print(g)
-if  c % 2 == 0:
-    k1 = True
+hour = int(input())
+if hour >= 12:
+    hour -= 12
+minute = int(input())
+mi = hour * 5
+min = 0
+mini = 0
+ho = hour + 1
+if mi > minute:
+    min = mi - minute
+    print(min)
 else:
-    k1 = False
-if not k1:
-    j = c // 2
-    print(k[j])
-if k1:
-  io =  c // 2
-  ia = io - 1
-  j = (k[io] + k[ia]) / 2
-  print(j)
-
-
+    mini = 60 - minute
+    ho *= 5
+    done = mini + ho
+    print (done)
 
